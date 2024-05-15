3a7e3dd7a0abbf09624ee07d92db95cfa7bcd965
Deepak bhardwaj <80276722+iamdeepak199@users.noreply.github.com>
2024-05-15 17:07:41 +0530

    Update Array_deletion.cpp
    For Invalid Position : We can simply decrement the size by 1 inside else block.
    using this syntax : 
     if(pos<=0 || pos>size){
                cout<<"Invailed Postion :"<<endl;
            }
            else{
        for(int i=pos-1;i<size-1;i++){
            arr[i]=arr[i+1];
        }
         size--;
            }
    
    it can work perfectly 💯

--- Array_deletion.cpp
+++ Array_deletion.cpp
@@ -22,11 +22,12 @@ int main() {
     if(pos<=0 || pos>size){
             cout<<"Invailed Postion :"<<endl;
         }
-        
+        else{
     for(int i=pos-1;i<size-1;i++){
         arr[i]=arr[i+1];
     }
      size--;
+        }
      cout<<"Updated array will be:"<<endl;
      for(int i=0;i<size;i++){
         
